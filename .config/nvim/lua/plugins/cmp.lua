a0e67ec59de336a86bea89d27eebf6a71711e464
arden <ardenhilario@gmail.com>
2024-09-13 22:46:50 +0800

    idk

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -50,6 +50,10 @@ return {
 				end, { "i", "s" }),
 			}),
 			sources = {
+				{
+					name = "lazydev",
+					group_index = 0,
+				},
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "path" },
