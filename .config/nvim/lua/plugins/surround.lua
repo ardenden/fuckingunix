7e7c1263bf2196e57e3bc882a7c1525fac0266a0
arden <ardenhilario@gmail.com>
2024-09-14 22:52:26 +0800

    names

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,8 +1,17 @@
 return {
-	"kylechui/nvim-surround",
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	event = "VeryLazy",
-	config = function()
-		require("nvim-surround").setup()
-	end,
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
+
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup()
+		end,
+	},
 }
