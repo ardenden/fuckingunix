69b51c4d0482fb7c00e9e7e0f735e89d6cff0029
arden <ardenhilario@gmail.com>
2024-09-14 22:33:33 +0800

    statusline colors

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -9,10 +9,22 @@ return {
 				return "recording @" .. recording_register
 			end
 		end
+		local theme = require("lualine.themes.codedark")
+		local i_a_bg = theme.insert.a.bg
+		local i_b_fg = theme.insert.b.fg
+		local n_a_bg = theme.normal.a.bg
+		local n_b_fg = theme.normal.b.fg
+		theme.normal.a.bg = i_a_bg
+		theme.normal.b.fg = i_b_fg
+		theme.insert.a.bg = n_a_bg
+		theme.insert.b.fg = n_b_fg
+		theme.normal.c.bg = "None"
+		theme.insert.c.bg = "None"
+		theme.replace.c.bg = "None"
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "auto",
+				theme = theme,
 				component_separators = "|",
 				section_separators = { left = "", right = "" },
 			},
