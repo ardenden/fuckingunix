acf192cbae0fb90f395df4e04e164629778fffff
arden <ardenhilario@gmail.com>
2024-09-24 01:59:04 +0800

    dressing

--- dot-config/nvim/lua/lazy-plugins.lua
+++ dot-config/nvim/lua/lazy-plugins.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
 	require("plugins/terminal"),
 	require("plugins/theme"),
 	require("plugins/transparent"),
+	require("plugins/select"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
