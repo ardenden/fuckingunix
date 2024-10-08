b47cec46ae1e307fe327afe777f5390b7a1517ef
arden <ardenhilario@gmail.com>
2024-10-08 16:07:23 +0800

    wrong place

--- dot-config/nvim/lua/plugins/notify.lua
+++ dot-config/nvim/lua/plugins/notify.lua
@@ -21,6 +21,17 @@ return {
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
+		routes = {
+			{
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = {
+					skip = true,
+				},
+			},
+		},
 	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
@@ -28,17 +39,6 @@ return {
 			"rcarriga/nvim-notify",
 			opts = {
 				background_colour = "#000000", -- for transparent background
-				routes = {
-					{
-						filter = {
-							event = "notify",
-							find = "No information available",
-						},
-						opts = {
-							skip = true,
-						},
-					},
-				},
 			},
 		},
 	},
