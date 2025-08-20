48cce52d64d49a621b91f8ae47e778d0ee305491
arden <ardenhilario@gmail.com>
2025-08-20 10:30:15 +0800

    wrong

--- dot-config/nvim/lua/options.lua
+++ dot-config/nvim/lua/options.lua
@@ -79,15 +79,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function()
-		if vim.fn.getline("$") ~= "" then
-			vim.fn.append("$", "")
-		end
-	end,
-})
-
 -- for ts @Decorator
 vim.api.nvim_set_hl(0, "@attribute.typescript", { link = "@operator" })
-
